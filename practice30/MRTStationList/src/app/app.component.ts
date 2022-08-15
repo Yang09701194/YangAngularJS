0a10e7d295505af834ead570f7f066eef7fddc05
Yang09701194 <mustnecessity@gmail.com>
2022-08-15 23:16:15 +0800

    20220815

---
+++ practice30/MRTStationList/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { stationList } from './station-list.const'
+
+declare var $:any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'MRTStationList';
+  //list = stationList;
+
+  getList = function(): any{
+    var ls : any= [] ;
+    $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: 'https://tcgmetro.blob.core.windows.net/stationnames/stations.json',
+      async: false,
+      success: function (data: any): any {
+        ls = data;
+      },
+      error: function (data: any): any {
+        alert('err: ' + data);
+      },
+    });
+    return ls;
+  }
+
+  list = this.getList();
+
+  constructor() {  // Constructor
+    setTimeout(() => {
+    this.list = this.getList();
+    console.log('updated' + new Date().getSeconds())
+  }, 5000);
+  }
+
+
+}
---
+++ practice30/MRTStationList/src/app/station-list.const.ts
@@ -0,0 +1,252 @@
+export const stationList = [
+    {
+      "Station": "七張站",
+      "Destination": "新店站",
+      "UpdateTime": "2022-08-15T18:03:03.06"
+    },
+    {
+      "Station": "三民高中站",
+      "Destination": "南勢角站",
+      "UpdateTime": "2022-08-15T18:02:51.62"
+    },
+    {
+      "Station": "三重站",
+      "Destination": "南勢角站",
+      "UpdateTime": "2022-08-15T18:02:46.653"
+    },
+    {
+      "Station": "大安站",
+      "Destination": "動物園站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "大安森林公園站",
+      "Destination": "淡水站",
+      "UpdateTime": "2022-08-15T18:03:12.91"
+    },
+    {
+      "Station": "大坪林站",
+      "Destination": "松山站",
+      "UpdateTime": "2022-08-15T18:02:43.013"
+    },
+    {
+      "Station": "大直站",
+      "Destination": "南港展覽館站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "大湖公園站",
+      "Destination": "動物園站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "小南門站",
+      "Destination": "新店站",
+      "UpdateTime": "2022-08-15T18:02:43.013"
+    },
+    {
+      "Station": "中山國小站",
+      "Destination": "南勢角站",
+      "UpdateTime": "2022-08-15T18:03:06.33"
+    },
+    {
+      "Station": "中山國中站",
+      "Destination": "南港展覽館站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "中山國中站",
+      "Destination": "動物園站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "內湖站",
+      "Destination": "動物園站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "六張犁站",
+      "Destination": "南港展覽館站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "文德站",
+      "Destination": "南港展覽館站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "文德站",
+      "Destination": "動物園站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "木柵站",
+      "Destination": "南港展覽館站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "木柵站",
+      "Destination": "動物園站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "北投站",
+      "Destination": "淡水站",
+      "UpdateTime": "2022-08-15T18:03:12.91"
+    },
+    {
+      "Station": "北門站",
+      "Destination": "新店站",
+      "UpdateTime": "2022-08-15T18:02:43.013"
+    },
+    {
+      "Station": "古亭站",
+      "Destination": "松山站",
+      "UpdateTime": "2022-08-15T18:03:03.06"
+    },
+    {
+      "Station": "台北小巨蛋站",
+      "Destination": "松山站",
+      "UpdateTime": "2022-08-15T18:03:03.06"
+    },
+    {
+      "Station": "台北車站",
+      "Destination": "南港展覽館站",
+      "UpdateTime": "2022-08-15T18:02:59.07"
+    },
+    {
+      "Station": "民權西路站",
+      "Destination": "蘆洲站",
+      "UpdateTime": "2022-08-15T18:03:01.757"
+    },
+    {
+      "Station": "永寧站",
+      "Destination": "頂埔站",
+      "UpdateTime": "2022-08-15T18:02:59.07"
+    },
+    {
+      "Station": "西門站",
+      "Destination": "南港展覽館站",
+      "UpdateTime": "2022-08-15T18:02:59.07"
+    },
+    {
+      "Station": "西湖站",
+      "Destination": "動物園站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "亞東醫院站",
+      "Destination": "南港展覽館站",
+      "UpdateTime": "2022-08-15T18:02:48.853"
+    },
+    {
+      "Station": "忠孝復興站",
+      "Destination": "動物園站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "忠孝新生站",
+      "Destination": "南勢角站",
+      "UpdateTime": "2022-08-15T18:02:57.08"
+    },
+    {
+      "Station": "東湖站",
+      "Destination": "動物園站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "松山機場站",
+      "Destination": "南港展覽館站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "松江南京站",
+      "Destination": "松山站",
+      "UpdateTime": "2022-08-15T18:02:43.013"
+    },
+    {
+      "Station": "南京復興站",
+      "Destination": "動物園站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "南港軟體園區站",
+      "Destination": "動物園站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "後山埤站",
+      "Destination": "亞東醫院站",
+      "UpdateTime": "2022-08-15T18:03:08.58"
+    },
+    {
+      "Station": "科技大樓站",
+      "Destination": "南港展覽館站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "紅樹林站",
+      "Destination": "淡水站",
+      "UpdateTime": "2022-08-15T18:03:12.91"
+    },
+    {
+      "Station": "海山站",
+      "Destination": "南港展覽館站",
+      "UpdateTime": "2022-08-15T18:03:08.58"
+    },
+    {
+      "Station": "景安站",
+      "Destination": "迴龍站",
+      "UpdateTime": "2022-08-15T18:02:46.653"
+    },
+    {
+      "Station": "港墘站",
+      "Destination": "動物園站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "善導寺站",
+      "Destination": "亞東醫院站",
+      "UpdateTime": "2022-08-15T18:02:59.07"
+    },
+    {
+      "Station": "善導寺站",
+      "Destination": "南港展覽館站",
+      "UpdateTime": "2022-08-15T18:02:48.853"
+    },
+    {
+      "Station": "圓山站",
+      "Destination": "北投站",
+      "UpdateTime": "2022-08-15T18:03:02.55"
+    },
+    {
+      "Station": "新莊站",
+      "Destination": "南勢角站",
+      "UpdateTime": "2022-08-15T18:02:46.653"
+    },
+    {
+      "Station": "葫洲站",
+      "Destination": "南港展覽館站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "劍南路站",
+      "Destination": "南港展覽館站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "劍南路站",
+      "Destination": "動物園站",
+      "UpdateTime": "2022-08-15T18:03:05"
+    },
+    {
+      "Station": "劍潭站",
+      "Destination": "淡水站",
+      "UpdateTime": "2022-08-15T18:02:53.383"
+    },
+    {
+      "Station": "雙連站",
+      "Destination": "淡水站",
+      "UpdateTime": "2022-08-15T18:03:02.55"
+    }
+  ]
